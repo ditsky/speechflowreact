0b028e245ec2105954ce149b1116ead1423d3825
ditsky <34258091+ditsky@users.noreply.github.com>
2018-11-13 14:39:39 -0500

    react routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 //import Pdf from './Pdf.js';
@@ -8,10 +9,28 @@ class App extends Component {
   render() {
     return (
     <div className="App">
-        <MyPdfViewer />
+        <Router>
+          <div>
+            <button>
+              <Link to='/pdf'>PDF Viewer</Link>
+            </button>
+            <button>
+              <Link to='/'>Home</Link>
+            </button>
+            <button>
+              <Link to='/video'>Video player</Link>
+            </button>
+            <Route exact path="/" component={Home} />
+            <Route path="/pdf" component={MyPdfViewer} />
+            <Route path ="/video" component={Video} />
+          </div>
+        </Router>
     </div>
 );
   }
 }
+
+const Home = () => <h2>Home</h2>;
+const Video = () => <h2>Video Player</h2>;
 
 export default App;
