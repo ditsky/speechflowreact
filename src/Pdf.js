5392e384c39b0ee7f3433ef7a7a134bc54a728ce
ditsky <34258091+ditsky@users.noreply.github.com>
2018-11-13 13:51:40 -0500

    working pdf viewer

---
+++ src/Pdf.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+//import pdf from './test.pdf';
+
+class Pdf extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <div>
+        <Document
+          file= "http://example.com/sample.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    );
+  }
+}
+
+export default Pdf;
---
+++ www/precache-manifest.d151896b909f65cd4e4c6c21d9187c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "90bdb843fec1360533cb9bce0dc72c99",
+    "url": "./static/media/trace.90bdb843.pdf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ef1bc457122eb57c23ba",
+    "url": "./static/js/main.ef1bc457.chunk.js"
+  },
+  {
+    "revision": "d3808fdad05c50012c8f",
+    "url": "./static/js/1.d3808fda.chunk.js"
+  },
+  {
+    "revision": "ef1bc457122eb57c23ba",
+    "url": "./static/css/main.31a3090a.chunk.css"
+  },
+  {
+    "revision": "650c08c45dddec12816f0e46de978d25",
+    "url": "./index.html"
+  }
+];
